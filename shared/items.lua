f726dd4e272d26847c6d4c78fc23fd1bbbbd7391
Devcon <devon.hopkins333@gmail.com>
2022-05-18 20:16:16 -0300

    added wheelchair

--- shared/items.lua
+++ shared/items.lua
@@ -778,4 +778,6 @@ QBShared.Items = {
         ["combinable"] = nil,
         ["description"] = "A Cherry GumBall from the local Store!"
     },
+    ["wheelchair"] 				     = {["name"] = "wheelchair", 			 	 ["label"] = "wheelchair", 		    ["weight"] = 500, 		["type"] = "item", 		["image"] = "wheelchair.png", 				    ["unique"] = true, 		["useable"] = true, 	["shouldClose"] = true,   ["combinable"] = nil,   ["description"] = "Crip Gang"},
+
 	}
